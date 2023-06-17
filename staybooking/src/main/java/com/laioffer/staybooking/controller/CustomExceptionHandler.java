a8cb9d67e7bbd2b8e72cc9ccf495221cbc1a59de
Di'an Fei <ryanfei514@gmail.com>
2023-06-17 14:02:15 -0400

    Token Based implementation in backend

--- staybooking/src/main/java/com/laioffer/staybooking/controller/CustomExceptionHandler.java
+++ staybooking/src/main/java/com/laioffer/staybooking/controller/CustomExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.laioffer.staybooking.controller;
 
 import com.laioffer.staybooking.exception.UserAlreadyExistException;
+import com.laioffer.staybooking.exception.UserNotExistException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,8 +12,15 @@ import org.springframework.web.context.request.WebRequest;
 public class CustomExceptionHandler {
 
     @ExceptionHandler(UserAlreadyExistException.class)
-    public final ResponseEntity<String> handleUserAlreadyExistExceptions(Exception ex, WebRequest request) {
+    public final ResponseEntity<String> handleUserAlreadyExistException(Exception ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
     }
+
+    @ExceptionHandler(UserNotExistException.class)
+    public final ResponseEntity<String> handleUserNotExistExceptions(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
 }
+
 
